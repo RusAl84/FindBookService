d4f2fd7c81bb02506395809f8196a4505e186648
Rusakov Aleksey <rusal@bk.ru>
2024-02-08 03:50:37 +0300

    fix

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,134 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <q-card class="chatmessages">
+      <div class="flexrow">
+        <div>
+          <q-card-section>
+            <div class="text-h6">
+              Введите описание для книги, которую вы хотите почитать:
+            </div>
+          </q-card-section>
+          <q-input
+            class="inputtext"
+            v-model="proc_text"
+            type="textarea"
+            autogrow
+            float-label="Введите описание для книги, которую вы хотите почитать"
+          />
+          <q-card-section>
+            <q-btn color="primary" label="Подобрать книгу" @click="onProc" />
+          </q-card-section>
+          <q-card-section>
+            <div class="text-h6">Результат:</div>
+            <q-input
+              class="inputtext"
+              v-model="resp_text"
+              type="textarea"
+              readonly
+              autogrow
+            />
+          </q-card-section>
+        </div></div
+    ></q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from "vue";
+import axios from "axios";
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+  data() {
+    return {
+      text: "",
+      resp_text: "",
+    };
+  },
+  methods: {
+    handleFileUpload() {
+      this.file = this.$refs.file.files[0];
+    },
+    fileUploaded({ files, xhr }) {
+      let data = JSON.parse(xhr.response);
+      this.chatmessages = data["text"];
+      this.filename = data["filename"];
+    },
+    async onProc() {
+      const response = await axios({
+        method: "post",
+        url: "http://127.0.0.1:5000/find_book",
+        data: {
+          text: this.text,
+        },
+      });
+      console.log(response);
+      this.resp_data = response.data;
+      this.resp_text = response.data.sintetic;
+    },
+    async onProcAdd() {
+      const response = await axios({
+        method: "post",
+        url: cfg.hostae + "get_pattern_add",
+        data: this.resp_data,
+      });
+      this.all_data = response.data;
+      console.log("this.resp_data");
+      console.log(this.all_data);
+    },
+    async onLoadDB() {
+      const response = await axios({
+        method: "get",
+        url: cfg.hostae + "load_db",
+        data: this.resp_data,
+      });
+      this.all_data = response.data;
+
+      console.log(this.all_data);
+    },
+    async onClearDB() {
+      const response = await axios({
+        method: "get",
+        url: cfg.hostae + "clear_db",
+      });
+      console.log(response);
+    },
+    async onFindCL() {
+      const response = await axios({
+        method: "post",
+        url: cfg.hostae + "findae",
+        data: {
+          filename: this.filename,
+          type: this.ttype,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      // console.log(response);
+      this.ae_data = response.data;
+      console.log("ae_data");
+      console.log(this.ae_data);
+    },
+  },
+};
 </script>
+
+<style lang="sass">
+.page
+  padding-bottom: 10px
+  padding-top: 10px
+  padding-left: 10px
+  padding-r: 10px
+
+.chatmessages
+  padding: 10px
+  margin: 10px
+.flexrow
+  display: flex
+  flex-flow: row wrap
+  justify-content: flex-start
+.editorclass
+  max-height: 500px
+.inputtext
+  width: 800px
+</style>
